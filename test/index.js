c18580e7e8a8a37b880121d5cf01713bae0f1023
Jonathan Ong <jonathanrichardong@gmail.com>
2013-04-25 15:12:04 -0700

    remove callbacks

--- test/index.js
+++ test/index.js
@@ -5,54 +5,48 @@ var block = require('../')
 var file = path.join(__dirname, 'template.html')
 
 describe('Block', function () {
-  it('should work with spaces', function (done) {
+  it('should work with spaces', function () {
     block('<div>{{ hello }}</div>').render({
       hello: 'a'
     }).should.equal('<div>a</div>')
-
-    done()
   })
 
-  it('should work without spaces', function (done) {
+  it('should work without spaces', function () {
     block('<div>{{hello}}</div>').render({
       hello: 'a'
     }).should.equal('<div>a</div>')
-
-    done()
   })
 
-  it('should not work with non-word names', function (done) {
-    block('<div>{{hell.oh}}</div>').render({
+  it('should work with multiple declarations of a block', function () {
+    block('<div>{{hello}}</div><div>{{hello}}</div><div>{{hello}}</div>').render({
       hello: 'a'
-    }).should.equal('<div>{{hell.oh}}</div>')
-
-    done()
+    }).should.equal('<div>a</div><div>a</div><div>a</div>')
   })
 
-  it('should work with multiple blocks', function (done) {
+  it('should work with multiple blocks', function () {
     block('<div>{{first}}</div><div>{{second}}</div>').render({
       first: 'a',
       second: 'b'
     }).should.equal('<div>a</div><div>b</div>')
-
-    done()
   })
 
-  it('should work with files', function (done) {
+  it('should work with files', function () {
     block.read(file).render({
       first: 'a',
       second: 'b'
     }).should.equal('<div>a</div>\n<div>b</div>')
-
-    done()
   })
 
-  it('should optionally minify files', function (done) {
+  it('should optionally minify files', function () {
     block.read(file, true).render({
       first: 'a',
       second: 'b'
     }).should.equal('<div>a</div><div>b</div>')
+  })
 
-    done()
+  it('should not work with non-word names', function () {
+    block('<div>{{hell.oh}}</div>').render({
+      hello: 'a'
+    }).should.equal('<div>{{hell.oh}}</div>')
   })
 })
